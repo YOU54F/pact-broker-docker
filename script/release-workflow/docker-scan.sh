5310a4a07d739d47adde9c8eeb68c75fa6d92dbf
Yousaf Nabi <you@saf.dev>
2023-04-06 14:35:50 +0100

    chore: revert local testing changes

--- script/release-workflow/docker-scan.sh
+++ script/release-workflow/docker-scan.sh
@@ -4,6 +4,6 @@ set -euo >/dev/null
 
 script_dir=$(cd "$(dirname $0)" && pwd)
 
-for arch in amd64 arm64 arm; do 
+for arch in amd64; do 
     ${script_dir}/../scan.sh ${DOCKER_IMAGE_ORG_AND_NAME}:latest-${arch}
 done
--- script/release-workflow/prepare-release.sh
+++ script/release-workflow/prepare-release.sh
@@ -5,7 +5,7 @@ set -euo >/dev/null
 docker_compose_files=$(find . -name "docker-compose*.yml" -not -name "*test*")
 
 for file in $docker_compose_files; do
-  cat $file | sed -e "s~image: you54f/pact-broker:.*~image: you54f/pact-broker:${TAG}~g" > dc-tmp
+  cat $file | sed -e "s~image: pactfoundation/pact-broker:.*~image: pactfoundation/pact-broker:${TAG}~g" > dc-tmp
   mv dc-tmp $file
 done
 
